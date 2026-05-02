671de3a0ea98069825810537b79aeb24d06e7474
rainndev <182114243+rainndev@users.noreply.github.com>
2026-05-02 23:23:21 +0800

    feat: add basic register, login, logout

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+       ]);
+
+        $user = User::create($validatedData);
+        if ($user) {
+            return redirect()->back()->with('success', 'Registration successful!');
+        } else {
+            return redirect()->back()->with('error', 'Registration failed. Please try again.');
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'loginname' => 'required|string|max:255',
+            'loginpassword' => 'required|min:6',
+       ]);
+
+        if (Auth::guard()->attempt([
+            'name' => $validatedData['loginname'],
+            'password' => $validatedData['loginpassword']
+        ])) {
+            return redirect('/dashboard');
+        } else {
+            return redirect()->back()->with('error', 'Login failed. Please check your credentials and try again.');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard()->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [UserController::class, 'index']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
+
