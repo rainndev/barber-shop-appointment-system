030106361dfcb582cef5ecfbdb6406df32789909
rainndev <182114243+rainndev@users.noreply.github.com>
2026-05-06 21:09:06 +0800

    feat: add logout functionality to navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,17 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <form method="POST" action="{{ route('logout') }}" class="me-3">
+                    @csrf
+
+                    <x-secondary-button
+                        onclick="event.preventDefault(); this.closest('form').submit();"
+                        class="text-xs"
+                    >
+                        {{ __('Log Out') }}
+                    </x-secondary-button>
+                </form>
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -84,13 +95,10 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                    <x-responsive-nav-link href="#" onclick="event.preventDefault(); this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
