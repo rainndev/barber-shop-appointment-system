1ad17c888521f1d269fabd06149bb180580fd523
rainndev <182114243+rainndev@users.noreply.github.com>
2026-05-07 22:23:44 +0800

    refactor: update CSS sources and remove unused Tailwind config

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./vendor/asantibanez/livewire-calendar/resources/**/*.blade.php",
-    ],
-};
