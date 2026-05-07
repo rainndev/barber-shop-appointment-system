e36ecc1417b7cf8aa56a0147807345f332f00766
rainndev <182114243+rainndev@users.noreply.github.com>
2026-05-07 11:15:30 +0800

    feat: add livewire-calendar views to Tailwind CSS configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.js",
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/asantibanez/livewire-calendar/resources/views/**/*.blade.php",
     ],
     theme: {
         extend: {
