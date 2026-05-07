3f868d075264806f16521f0bec97360a611a6721
rainndev <182114243+rainndev@users.noreply.github.com>
2026-05-07 17:45:30 +0800

    fix: update dropdown content class for improved styling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,7 +38,7 @@
                         </button>
                     </x-slot>
 
-                    <x-slot name="content">
+                    <x-slot name="content" >
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
